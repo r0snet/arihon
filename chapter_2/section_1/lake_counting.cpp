432214e39d06f3c61ef7987430eb0edd1ae6116c
{{r0anet} <{{Unzen.zn49@gmail.com}}>
2020-09-08 10:53:38 +0900

    微修正

--- chapter_2/section_1/lake_counting.cpp
+++ chapter_2/section_1/lake_counting.cpp
@@ -1,49 +1,60 @@
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<cmath>
-#include<vector>
-#include<map>
-#include<set>
-#include<string>
-#include<queue>
-#include<stack>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <queue>
+#include <stack>
 using namespace std;
 #define MOD 1000000007
-#define INF (1<<29)
-#define LINF (1LL<<60)
+#define INF (1 << 29)
+#define LINF (1LL << 60)
 #define EPS (1e-10)
 
 static const int MAX_N = 100;
 static const int MAX_M = 100;
-int n,m;
+int n, m;
 char field[MAX_N][MAX_M];
 
-void dfs(int x, int y){
-    field[x][y] = '.';
-    for(int dx=-1; dx<=1; dx++){
-        for(int dy=-1; dy<=1; dy++){
+void dfs(int x, int y)
+{
+    field[x][y] = '.'; //現在位置を.に
+    for (int dx = -1; dx <= 1; dx++)
+    {
+        for (int dy = -1; dy <= 1; dy++)
+        {
             int nx = x + dx;
             int ny = y + dy;
-            if(0<=nx && nx<n && 0<=ny && ny<m && field[nx][ny]=='W') dfs(nx,ny);
+            if (0 <= nx && nx < n && 0 <= ny && ny < m && field[nx][ny] == 'W')
+                dfs(nx, ny);
+            // Wを八近傍に発見したらそこに移動して再帰
         }
     }
-    return ;
+    return;
 }
 
-int main(){
+int main()
+{
     cin >> n >> m;
-    for (int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             cin >> field[i][j];
         }
     }
     int res = 0;
-    for (int i  = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(field[i][j]=='W'){
-                dfs(i,j);
-                res++;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (field[i][j] == 'W')
+            {
+                dfs(i, j);
+                res++; // 再帰まで含めて一回としたdfsの呼び出し回数が水たまりの個数
             }
         }
     }
--- chapter_2/section_3/knapsack_inf.cpp
+++ chapter_2/section_3/knapsack_inf.cpp
@@ -1,43 +1,48 @@
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<cmath>
-#include<vector>
-#include<map>
-#include<set>
-#include<cstring>
-#include<queue>
-#include<stack>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <cstring>
+#include <queue>
+#include <stack>
 using namespace std;
 #define MOD 1000000007
-#define INF (1<<29)
-#define LINF (1LL<<60)
+#define INF (1 << 29)
+#define LINF (1LL << 60)
 #define EPS (1e-10)
-using ll = long long;
 static const int MAX_N = 101;
 
-int n,W;
-int w[MAX_N],v[MAX_N];
+int n, W;
+int w[MAX_N], v[MAX_N];
 int dp[2][MAX_N];
 
 //dp[i+1][j]:i番目までの品物から重さの総和がj以下となるように選んだ時の価値の最大値
-int main(){
+int main()
+{
     cin >> n >> W;
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> w[i] >> v[i];
     }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<=W; j++){
-            if(j<w[i]){
-                dp[(i+1)&1][j]=dp[i&1][j];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= W; j++)
+        {
+            if (j < w[i])
+            {
+                dp[(i + 1) & 1][j] = dp[i & 1][j];
             }
-            else{
-                dp[(i+1)&1][j]=max(dp[i&1][j], dp[(i+1)&1][j-w[i]]+v[i]);
+            else
+            {
+                dp[(i + 1) & 1][j] = max(dp[i & 1][j], dp[(i + 1) & 1][j - w[i]] + v[i]);
                 //右辺第二項がknapsack_memorize2と違う!
             }
         }
     }
-    cout << dp[n&1][W];
+    cout << dp[n & 1][W];
     return 0;
 }
 //m mod 2^nは m&(2~n-1) により求まる!
